9e79115527279fdbed246dd520980e565c7f9b9a
nam hoang <hihihehehd1122@hotmail.com>
2020-09-16 02:24:32 +0700

    create responsitory

---
+++ Ecommerce.Repository/Interfaces/IUserProfileResponsitory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ecommerce.Domain.Models;
+namespace Ecommerce.Repository.Interfaces
+{
+    public interface IUserProfileResponsitory : IRepository<UserProfile>
+    {
+
+        //IEnumerable<User> GetAll();
+        //User GetById(int id);
+        void Update(UserProfile profile);
+        void Delete(Guid id);
+    }
+}
---
+++ Ecommerce.Repository/RoleReponsitory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ecommerce.Domain;
+using Ecommerce.Domain.Models;
+using Ecommerce.Repository.Interfaces;
+
+namespace Ecommerce.Repository
+{
+    public class RoleReponsitory : BaseRepository<Role>, IRoleReponsitory
+    {
+        public RoleReponsitory(EcommerceDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
